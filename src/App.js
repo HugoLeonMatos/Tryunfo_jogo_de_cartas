4bf389ff070601f632e955d07d858b80eb85293b
Hugo Leon <hugoleon@outlook.com>
2023-05-28 13:36:32 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -21,9 +21,12 @@ class App extends React.Component {
     const value = (target.type === 'checkbox') ? target.checked : target.value;
     console.log(name);
     console.log(value);
-    this.setState({
-      [name]: value,
-    });
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.buttonVerifyVoid(),
+    );
   };
 
   // buttonVerifyVoid = () => {
@@ -33,6 +36,47 @@ class App extends React.Component {
   //     isSaveButtonDisabled = true;
   //   }
   // };
+  buttonVerifyVoid = () => {
+    const attrLim = 90;
+    const attrTotLim = 210;
+    const minValue = 0;
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+    } = this.state;
+
+    let isSaveButtonDisabled = false;
+
+    if (
+      cardName === ''
+    || cardDescription === ''
+    || cardImage === ''
+    || cardRare === ''
+    ) {
+      isSaveButtonDisabled = true;
+    }
+
+    if (
+      Number(cardAttr1) > attrLim
+    || Number(cardAttr1) < minValue
+    || Number(cardAttr2) > attrLim
+    || Number(cardAttr2) < minValue
+    || Number(cardAttr3) > attrLim
+    || Number(cardAttr3) < minValue
+    || Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) > attrTotLim
+    ) {
+      isSaveButtonDisabled = true;
+    }
+
+    this.setState({
+      isSaveButtonDisabled,
+    });
+  };
 
   render() {
     return (

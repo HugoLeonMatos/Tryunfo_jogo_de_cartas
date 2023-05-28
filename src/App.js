89a743e083c2ada732e66c6b95777f4c2708f53b
Hugo Leon <hugoleon@outlook.com>
2023-05-28 11:46:26 -0300

    lint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Form from './components/Form';
 import Card from './components/Card';
+import Form from './components/Form';
 
 class App extends React.Component {
   state = {
